bf6b912360141766819af822f37a9f6a170d93ee
saltzberg <saltzberg.salilab.org>
2019-02-28 16:07:41 -0800

    remove copied files

--- SSEThread_2019/dnapkcs_disordered_domain/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains the source code and data to replicate and explore the methods as described in Saltzberg DJ, Hepburn M, et. al 2019. DOI:
--- SSEThread_2019/dnapkcs_disordered_domain/multi_enumerate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-../../../../BUILD/setup_environment.sh python modeling_enumerate_multi.py 0 100 &
-../../../../BUILD/setup_environment.sh python modeling_enumerate_multi.py 1 100 &
-../../../../BUILD/setup_environment.sh python modeling_enumerate_multi.py 2 100 &
-../../../../BUILD/setup_environment.sh python modeling_enumerate_multi.py 3 100 &
-
